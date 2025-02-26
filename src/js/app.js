a0c3dbec23257b94068f9346029b0bb3026add36
thieleju <41455100+thieleju@users.noreply.github.com>
2025-02-26 21:55:13 +0100

    refactor: Move functions from game.js to app.js and dom.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 import { STATE_DEFAULT, INTERVAL_MS } from "./constants.js"
 import { fetchGames, fetchAllCurrentRatings } from "./api.js"
-import { getScore, animateEloDiff } from "./game.js"
 import {
   getDomElements,
   updateWld,
   updateRatingDiff,
-  setGameModeButtonActive
+  setGameModeButtonActive,
+  animateEloDiff
 } from "./dom.js"
 
 let state = JSON.parse(JSON.stringify(STATE_DEFAULT))
@@ -113,6 +113,72 @@ async function start() {
   }
 
   startInterval()
+}
+
+/**
+ * Maps the result of a game to a score value
+ * @param {string} result - The result of the game (win, draw, loss)
+ * @returns {number} The score value for the result
+ */
+export function mapResult(result) {
+  if (!result) return 0
+  if (result === "win") return 1
+  if (result === "draw") return 0.5
+  return 0
+}
+
+/**
+ * Calculate the score based on the games
+ * @param {object[]} games - The games to calculate the score from
+ * @param {object} state - The current state
+ * @param {Set} processedGameUUIDs - The set of processed game UUIDs
+ * @param {number} scriptStartTime - The start time of the script
+ * @returns {object} The updated score
+ */
+function getScore(games, state, processedGameUUIDs, scriptStartTime) {
+  // Create a copy of the current score
+  const newScore = state.modes[state.gameMode].score
+  if (!games || games.length === 0) return newScore
+
+  // Only consider games that ended after the start time and match the current mode, and skip already processed games
+  const gamesToCheck = games.filter(
+    (game) =>
+      game.end_time > scriptStartTime &&
+      game.time_class === state.gameMode &&
+      !processedGameUUIDs.has(game.uuid)
+  )
+
+  for (const game of gamesToCheck) {
+    let userResult = null
+    if (game.white.username.toLowerCase() === state.username.toLowerCase()) {
+      userResult = mapResult(game.white.result)
+    } else if (
+      game.black.username.toLowerCase() === state.username.toLowerCase()
+    ) {
+      userResult = mapResult(game.black.result)
+    }
+
+    switch (userResult) {
+      case 1: {
+        newScore.wins += 1
+        break
+      }
+      case 0: {
+        newScore.losses += 1
+        break
+      }
+      case 0.5: {
+        newScore.draws += 1
+        break
+      }
+      default: {
+        throw new Error("Invalid user result")
+      }
+    }
+    // Mark the game as processed
+    processedGameUUIDs.add(game.uuid)
+  }
+  return newScore
 }
 
 /**
