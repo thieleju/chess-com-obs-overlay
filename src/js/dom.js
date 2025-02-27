5c1e60aea3a04881795e66b2ddf5d9ebbdfd4074
thieleju <41455100+thieleju@users.noreply.github.com>
2025-02-28 00:37:48 +0100

    feat: Add settings and refactor app.js

--- src/js/dom.js
+++ src/js/dom.js
@@ -11,12 +11,12 @@ import {
  */
 export function getDomElements() {
   return {
+    body: document.body,
     wld: document.getElementById("wld"),
     ratingDiff: document.getElementById("ratingDiff"),
     rating: document.querySelector(".rating"),
     editMode: document.getElementById("editMode"),
     reset: document.getElementById("reset"),
-    global: document.getElementById("global"),
     errorMessage: document.getElementById("errorMessage"),
     successMessage: document.getElementById("successMessage"),
     usernameInput: document.getElementById("usernameInput"),
@@ -32,21 +32,21 @@ export function getDomElements() {
 
 /**
  * Update the rating
+ * @param {object} domElements - The DOM elements
  * @param {number} wins - The number of wins
  * @param {number} losses - The number of losses
  * @param {number} draws - The number of draws
- * @param {object} domElements - The DOM elements
  */
-export function updateWld(wins, losses, draws, domElements) {
-  domElements.wld.innerHTML = `${wins} / ${losses} / ${draws}`
+export function setWld(domElements, wins, losses, draws) {
+  domElements.wld.innerHTML = `${wins ?? 0} / ${losses ?? 0} / ${draws ?? 0}`
 }
 
 /**
  * Update the rating difference
- * @param {number} ratingDiff - The rating difference
  * @param {object} domElements - The DOM elements
+ * @param {number} ratingDiff - The rating difference
  */
-export function updateRatingDiff(ratingDiff, domElements) {
+export function setRatingDiff(domElements, ratingDiff) {
   if (ratingDiff === 0) {
     domElements.ratingDiff.innerHTML = `+${ratingDiff}`
     domElements.ratingDiff.style.color = COLOR_WHITE
@@ -72,13 +72,72 @@ export function setGameModeButtonActive(domElements, mode) {
 }
 
 /**
+ * Show or hide the rating difference
+ * @param {object} domElements - The DOM elements
+ * @param {boolean} showEloDiff - The show rating difference
+ */
+export function setShowRatingDiff(domElements, showEloDiff) {
+  domElements.toggleElo.checked = showEloDiff
+  domElements.eloDiffContainer.style.display = showEloDiff ? "block" : "none"
+}
+
+/**
+ * Set the edit mode
+ * @param {object} domElements - The DOM elements
+ * @param {boolean} editMode - The edit mode
+ */
+export function setEditMode(domElements, editMode) {
+  domElements.editMode.style.display = editMode ? "block" : "none"
+}
+
+/**
+ * Set the username input
+ * @param {object} domElements - The DOM elements
+ * @param {string} username - The username
+ */
+export function setUserNameInput(domElements, username) {
+  domElements.usernameInput.value = username
+}
+
+/**
+ * Show a success message (bit hacky I know)
+ * @param {object} elements - The DOM elements
+ * @param {string} message - The message to show
+ */
+export function showSuccessMessage(elements, message) {
+  // Delete the old timeout if it exists
+  if (elements.successMessage.dataset.timeoutId) {
+    clearTimeout(elements.successMessage.dataset.timeoutId)
+  }
+
+  elements.successMessage.innerHTML = message
+
+  // Set a new timeout to remove the message after 1 second
+  const timeoutId = setTimeout(() => {
+    elements.successMessage.innerHTML = ""
+    delete elements.successMessage.dataset.timeoutId
+  }, 1000)
+
+  elements.successMessage.dataset.timeoutId = timeoutId
+}
+
+/**
+ * Show an error message
+ * @param {object} elements - The DOM elements
+ * @param {string} message - The message to show
+ */
+export function showErrorMessage(elements, message) {
+  elements.errorMessage.innerHTML = message
+}
+
+/**
  * Animate the difference in Elo ratings
+ * @param {object} elements - The DOM elements
  * @param {number} newEloDiff - The new Elo difference
  * @param {number} lastRatingDiff - The last rating difference
- * @param {object} elements - The DOM elements
  * @param {Function} callback - The callback function to update the rating difference
  */
-export function animateEloDiff(newEloDiff, lastRatingDiff, elements, callback) {
+export function animateEloDiff(elements, newEloDiff, lastRatingDiff, callback) {
   const start = performance.now()
   const initialDiff = lastRatingDiff
 
@@ -88,7 +147,7 @@ export function animateEloDiff(newEloDiff, lastRatingDiff, elements, callback) {
       initialDiff + timeFraction * (newEloDiff - initialDiff)
     )
 
-    callback(ratingDiff, elements)
+    callback(elements, ratingDiff)
 
     if (timeFraction < 1) requestAnimationFrame(animate)
   })
